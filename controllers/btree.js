d7fb273c93be49ea46edf0e44dcb5117a7cd69fb
Duc Thu <14520908@gm.uit.edu.vn>
2018-08-28 17:55:44 +0700

    new

--- controllers/btree.js
+++ controllers/btree.js
@@ -1,20 +1,64 @@
 const async = require('async');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const config = require('../lib/config');
+
 
 exports.initialize = function (req, res) {
-    async.waterfall([
-        buildTree(err),
-        saveTree(),
-        function () {
 
-        }
-    ]);
+    async.waterfall([
+            convert,
+            buildTree,
+            saveTree],
+        function (err, result) {
+            if (err) {
+                // console.log(err);
+            }
+            return res.render('upload', {result});
+        });
 };
 
-function buildTree() {
-
+function convert(callback) {
+    var parser = new xml2js.Parser();
+    fs.readFile('./xml/' + config.get('newest'), function (err, data) {
+        parser.parseString(data, function (err, result) {
+            callback(null, result.dictionary.record);
+        });
+    });
 }
 
-function saveTree() {
+function buildTree(converted, callback) {
+    //TODO: Build Tree here
+    var bTree = {};
+    converted.map(function (v,i,a) {
+        words = a[i].word[0];
+        for(var t = 0;  t < words.length; t++){
+            var cur = 'bTree.' + words.substring(0,t+1).split('').join('.');
+            var pre = cur.slice(0,-2);
+            var k = words[t];
+            if(!pre.hasOwnProperty(k)){
+                eval(cur + '= {' + k + ':k}');
+            }
+        }
+    });
 
+    callback(null, bTree);
 }
 
+function saveTree(bTree, callback) {
+    const content = JSON.stringify(bTree);
+    // writeStream.on('finish', function () {
+    // });
+
+    // fs.writeFile(__dirname + "/../json/btree/converted.json", content,'utf8', function (err) {
+    //     if (err) {
+    //         return callback(err, 'The file could not be saved!');
+    //     }
+    // var writeStream = fs.createWriteStream(__dirname + "/../json/btree/converted.json");
+    // writeStream.write(content, 'utf8');
+    callback(null, content);
+
+    // });
+}
+
+//`chay for lay 1 tu, trong 1 tu for tung chu. kiem tra key cua object[chu]. neu !=null thi continue, = null thi lay index cua chu~: chay tu cuoi tu den index, tao object moi. roi add vao object root.`
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const btree = require('./controllers/btree');
 const handle = require('./controllers/handle');
 const path = require('path');
+var bodyParser = require('body-parser');
 var multer = require('multer');
 var config = require('./lib/config');
-
 var express = require('express');
-var app = express();
-app.listen(3000);
 
+var app = express();
+
+app.listen(3000);
 app.use(express.static(path.join(__dirname, 'client')));
-var bodyParser = require('body-parser');
-var parseForm = bodyParser.urlencoded({extended: false});
+var parseForm = bodyParser.urlencoded({
+    extended: false,
+    limit: '200mb',
+});
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
